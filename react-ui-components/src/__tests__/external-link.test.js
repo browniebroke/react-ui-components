dc33a2285d72adcd0bc2b5d622d3b3141c2ba983
Bruno Alla <bruno.alla@festicket.com>
2020-11-15 18:17:52 +0000

    feat: rename package and add first component

---
+++ react-ui-components/src/__tests__/external-link.test.js
@@ -0,0 +1,13 @@
+// external-link.test.js
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ExternalLink from '../external-link'
+
+describe('ExternalLink component', () => {
+  test('should render correctly', () => {
+    let tree = renderer
+      .create(<ExternalLink title="Home page" to="/" />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
