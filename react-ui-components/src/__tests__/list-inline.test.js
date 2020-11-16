233062e3b73165005db5175c80b65d59aa48fd22
Bruno Alla <bruno.alla@festicket.com>
2020-11-16 15:40:33 +0000

    fix: export theming

--- react-ui-components/src/__tests__/list-inline.test.js
+++ react-ui-components/src/__tests__/list-inline.test.js
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components'
 import renderer from 'react-test-renderer'
 
 import ListInline from '../list-inline'
-import theme from '../theme'
+import styledComponentsTheme from '../styled-components-theme'
 
 describe('ListInline component', () => {
   test('should render correctly with children', () => {
     let tree = renderer
       .create(
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={styledComponentsTheme}>
           <ListInline>
             <div>This</div>
             <div>is</div>
@@ -26,7 +26,7 @@ describe('ListInline component', () => {
   test('should render correctly without children', () => {
     let tree = renderer
       .create(
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={styledComponentsTheme}>
           <ListInline />
         </ThemeProvider>
       )
@@ -37,7 +37,7 @@ describe('ListInline component', () => {
   test('should render with custom padding', () => {
     let tree = renderer
       .create(
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={styledComponentsTheme}>
           <ListInline padding={0}>
             <div>This</div>
             <div>is</div>
---
+++ react-ui-components/src/styled-components-theme.js
@@ -0,0 +1,34 @@
+import typography from './typography'
+
+const styledComponentsTheme = {
+  colors: {
+    primary: '#0d6efd',
+    primaryDark: '#0b4fb1',
+    secondary: '#6c757d',
+    secondaryDark: '#495057',
+  },
+  gutterWidth: '15px',
+  spacings: [
+    typography.rhythm(1 / 6),
+    typography.rhythm(1 / 3),
+    typography.rhythm(2 / 3),
+    typography.rhythm(1),
+    typography.rhythm(2),
+  ],
+  gridBreakpoints: {
+    xs: '0',
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+  },
+  // Note: all the keys in the below object should exist above
+  containersMaxWidth: {
+    sm: '540px',
+    md: '720px',
+    lg: '960px',
+    xl: '1140px',
+  },
+}
+
+export default styledComponentsTheme
