6a5f2948d1bf5c750df246acc61b03deafec66bd
Bruno Alla <browniebroke@users.noreply.github.com>
2021-02-16 23:11:09 +0000

    fix: types for external link component

--- test/external-link.test.tsx
+++ test/external-link.test.tsx
@@ -4,9 +4,32 @@ import renderer from 'react-test-renderer'
 import { ExternalLink } from '../src'
 
 describe('ExternalLink component', () => {
-  test('should render correctly', () => {
+  test('should render correctly with no children', () => {
     let tree = renderer
       .create(<ExternalLink title="Home page" to="/" />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('should render correctly with a text node as child', () => {
+    let tree = renderer
+      .create(
+        <ExternalLink title="Home page" to="/">
+          Home Page
+        </ExternalLink>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('should render correctly with another component as child', () => {
+    const MyCustomDiv = () => <div>Custom thing</div>
+    let tree = renderer
+      .create(
+        <ExternalLink title="Home page" to="/">
+          <MyCustomDiv />
+        </ExternalLink>
+      )
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
