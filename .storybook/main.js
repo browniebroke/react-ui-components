cac359f497aafe8072362aa1b969b4669d24c07d
Bruno Alla <alla.brunoo@gmail.com>
2021-06-01 08:01:32 +0000

    chore: workaround for react docgen issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,7 @@ module.exports = {
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+    // https://github.com/styleguidist/react-docgen-typescript/issues/356
+    reactDocgen: 'none',
   },
 }
