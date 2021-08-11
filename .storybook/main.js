b0b8812f442bf131026f6854a524ebd5fed69bc3
Bruno Alla <browniebroke@users.noreply.github.com>
2021-08-11 09:49:23 +0200

    chore: configure Storybook to handle numeric separators
    The v4 of color introduced some uses of underscore in numeric separators.
    This breaks Storybook as its default Babel config doesn't understand them.
    Update the Babel config to include the numeric separator proposal.
    
    https://github.com/Qix-/color/issues/204
    https://storybook.js.org/docs/react/configure/babel
    https://babeljs.io/docs/en/babel-plugin-proposal-numeric-separator

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,18 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  babel: async (options) => {
+    updatedOptions = {
+      ...options,
+      // any extra options you want to set
+      plugins: [
+        ...options.plugins,
+        require.resolve('@babel/plugin-proposal-numeric-separator'),
+      ],
+    }
+    console.log(updatedOptions)
+    return updatedOptions
+  },
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
